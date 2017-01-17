4642a6cdaeaae054b409aabfd8baeba6c987fc30
ThienLV <ohmygodvt95@gmail.com>
2017-01-17 11:46:07 +0700

    Show list books on Homepage

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,12 @@
 class StaticPagesController < ApplicationController
+  before_action :categories, only: [:show]
+
   def show
     if valid_page?
-      render template: "static_pages/#{params[:page]}"
+      @books = Book.all
+        .paginate page: params[:page], per_page: Settings.static_pages.per_page
+
+      render template: "static_pages/#{params[:act]}"
     else
       render file: "public/404.html", status: :not_found
     end
@@ -9,6 +14,10 @@ class StaticPagesController < ApplicationController
 
   private
   def valid_page?
-    File.exist? Pathname.new Rails.root + "app/views/static_pages/#{params[:page]}.html.erb"
+    File.exist? Pathname.new Rails.root + "app/views/static_pages/#{params[:act]}.html.erb"
+  end
+
+  def categories
+    @categories = Category.all
   end
 end
