053f3b20f58d9f67c613edb8d4b28d6cffacdbe7
Le Tat Tung <letattungtb@gmail.com>
2017-02-14 00:49:51 +0700

    admin show detail book

--- app/controllers/admin/books_controller.rb
+++ app/controllers/admin/books_controller.rb
@@ -1,5 +1,19 @@
 class Admin::BooksController < Admin::AdminController
+  before_action :find_book, only: [:show]
+
   def index
     @books = Book.all
+  end
+
+  def show
+  end
+
+  private
+  def find_book
+    @book = Book.find_by id: params[:id]
+    unless @book
+      flash[:danger] = t "books.error.book_not_found"
+      redirect_to root_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :admin do
     root to: "books#index", as: :root
-    resources :books, only: :index
+    resources :books, only: [:index, :show]
     resources :users, only: :index
   end
 
