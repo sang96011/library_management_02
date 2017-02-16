babd95bb2582d016d005131c5850eb4173bb856e
Le Tat Tung <letattungtb@gmail.com>
2017-02-16 10:54:52 +0700

    Export list authors to excel by query search

--- app/controllers/admin/authors_controller.rb
+++ app/controllers/admin/authors_controller.rb
@@ -5,6 +5,12 @@ class Admin::AuthorsController < Admin::AdminController
     else
       Author.all
     end.paginate page: params[:page], per_page: Settings.per_page
+    respond_to do |format|
+      format.js
+      format.html
+      format.csv {send_data @authors.to_csv}
+      format.xls {send_data @authors.to_csv(col_sep: "\t")}
+     end
   end
 
   def show
